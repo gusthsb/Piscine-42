5ff6cccb946ed278368a1d4d6f50eb31fbea7191
gusthsb <gustavo.hdsbrito@gmail.com>
2026-03-24 17:09:04 -0300

    piscine

--- rush00/ex00/rush03.c
+++ rush00/ex00/rush03.c
@@ -14,28 +14,28 @@ void	ft_putchar(char c);
 
 void	rush(int x, int y)
 {
-	int	linha;
-	int	coluna;
+	int	row;
+	int	col;
 
 	if (x <= 0 || y <= 0)
 		return ;
-	linha = 1;
-	while (linha <= y)
+	row = 1;
+	while (row <= y)
 	{
-		coluna = 1;
-		while (coluna <= x)
+		col = 1;
+		while (col <= x)
 		{
-			if ((linha == 1 && coluna == 1) || (linha == y && coluna == 1))
+			if ((row == 1 && col == 1) || (row == y && col == 1))
 				ft_putchar('A');
-			else if ((linha == 1 && coluna == x) || (linha == y && coluna == x))
+			else if ((row == 1 && col == x) || (row == y && col == x))
 				ft_putchar('C');
-			else if (linha == 1 || linha == y || coluna == 1 || coluna == x)
+			else if (row == 1 || row == y || col == 1 || col == x)
 				ft_putchar('B');
 			else
 				ft_putchar(' ');
-			coluna++;
+			col++;
 		}
 		ft_putchar('\n');
-		linha++;
+		row++;
 	}
 }
